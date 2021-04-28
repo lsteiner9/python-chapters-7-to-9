416683a3ad3bb94a9298d1ef542627d0ef01b9ac
lsteiner9 <lsteiner1@cnm.edu>
2021-04-28 17:46:30 -0600

    Revised and improved result tracking for rball.py

--- chapter9/rball.py
+++ chapter9/rball.py
@@ -39,28 +39,49 @@ def sim_one_game(prob_a, prob_b):
 
 
 def sim_n_games(n, prob_a, prob_b):
-    wins_a = wins_b = 0
+    wins_a = wins_b = shutouts_a = shutouts_b = 0
     for i in range(n):
         score_a, score_b = sim_one_game(prob_a, prob_b)
         if score_a > score_b:
+            print("Game %d: A wins. Score for A: %d. Score for B: %d."
+                  % (i + 1, score_a, score_b))
             wins_a += 1
+            if score_b == 0:
+                shutouts_a += 1
         else:
+            print("Game %d: B wins. Score for A: %d. Score for B: %d."
+                  % (i + 1, score_a, score_b))
             wins_b += 1
-    return wins_a, wins_b
+            if score_a == 0:
+                shutouts_b += 1
+
+    return wins_a, wins_b, shutouts_a, shutouts_b
 
 
-def print_summary(wins_a, wins_b):
+def print_summary(wins_a, wins_b, shutouts_a, shutouts_b):
     n = wins_a + wins_b
     print("\nGames simulated: %d" % n)
-    print("Wins for A: %d (%0.1f%%)" % (wins_a, float(wins_a) / n))
-    print("Wins for B: %d (%0.1f%%)" % (wins_b, float(wins_b) / n))
+    if wins_a > 0:
+        print("Wins for A: %d (%0.1f%%). Shutouts for A: %d (%.1f%% of wins)."
+              % (wins_a, float(wins_a) * 100 / n, shutouts_a,
+                 float(shutouts_a) * 100 / wins_a))
+    else:
+        print("Wins for A: %d (%.1f%%). Shutouts for A: 0 (0.0%% of wins)."
+              % (wins_a, float(wins_a) * 100 / n))
+    if wins_b > 0:
+        print("Wins for B: %d (%.1f%%). Shutouts for B: %d (%.1f%% of wins)."
+              % (wins_b, float(wins_b) * 100 / n, shutouts_b,
+                 float(shutouts_b) * 100 / wins_b))
+    else:
+        print("Wins for B: %d (%.1f%%). Shutouts for B: 0 (0.0%% of wins)."
+              % (wins_b, float(wins_b) * 100 / n))
 
 
 def main():
     print_intro()
     prob_a, prob_b, n = get_inputs()
-    wins_a, wins_b = sim_n_games(n, prob_a, prob_b)
-    print_summary(wins_a, wins_b)
+    wins_a, wins_b, shutouts_a, shutouts_b = sim_n_games(n, prob_a, prob_b)
+    print_summary(wins_a, wins_b, shutouts_a, shutouts_b)
 
 
 if __name__ == '__main__':
